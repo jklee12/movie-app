32377e2d823286a9f367107c98683e5656b42ed8
jklee12 <junggnl@naver.com>
2024-09-05 12:30:51 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from '../core/jkl'
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'JKL / LeeJungKeun',
+  email: 'junggnl@naver.com',
+  blog: 'https://junggnl.tistory.com',
+  github: 'https://github.com/jklee12',
+  repository: 'https://github.com/jklee12/movie-app'
+})
